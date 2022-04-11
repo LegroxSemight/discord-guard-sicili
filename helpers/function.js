93d5fd8626fd072c827857ae83e54416608d8bc4
Legrox Semight <83615795+LegroxSemight@users.noreply.github.com>
2022-04-11 03:33:22 -0700

    Create function.js

---
+++ helpers/function.js
@@ -0,0 +1,34 @@
+const operation = require(`./transactions`)
+client.process = async (guildID, userID, type,text,time) => {
+    let id = await operation.find({ guildID });
+    id = id ? id.length + 1 : 1;
+    return await new operation({ id, userID, guildID, type, text, time}).save();
+};
+Date.prototype.toTurkishFormatDate = function (format) {
+    let date = this,
+      day = date.getDate(),
+      weekDay = date.getDay(),
+      month = date.getMonth(),
+      year = date.getFullYear(),
+      hours = date.getHours(),
+      minutes = date.getMinutes(),
+      seconds = date.getSeconds();
+    let monthNames = new Array("Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık");
+    let dayNames = new Array("Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi");
+    if (!format) {
+      format = "dd MM yyyy - HH:ii:ss";
+    };
+    format = format.replace("mm", month.toString().padStart(2, "0"));
+    format = format.replace("MM", monthNames[month]);
+    if (format.indexOf("yyyy") > -1) {
+      format = format.replace("yyyy", year.toString());
+    } else if (format.indexOf("yy") > -1) {
+      format = format.replace("yy", year.toString().substr(2, 2));
+    };
+    format = format.replace("dd", day.toString().padStart(2, "0"));
+    format = format.replace("DD", dayNames[weekDay]);
+    if (format.indexOf("HH") > -1) format = format.replace("HH", hours.toString().replace(/^(\d)$/, '0$1'));
+    if (format.indexOf("ii") > -1) format = format.replace("ii", minutes.toString().replace(/^(\d)$/, '0$1'));
+    if (format.indexOf("ss") > -1) format = format.replace("ss", seconds.toString().replace(/^(\d)$/, '0$1'));
+    return format;
+  };
